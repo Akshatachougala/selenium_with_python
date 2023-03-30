c09ce2acb4eeed61b2b235cef5da42156492b261
Akshata Chougula <Akshata Chougula@SMTC-R-0156>
2023-03-30 16:36:00 +0530

    updated

--- intro/Demo.py
+++ intro/Demo.py
@@ -1,1 +1,9 @@
 print("hello world")
+from selenium import webdriver
+driver=webdriver.Chrome()
+driver.get("https://www.google.com")
+time.sleep(5)
+print(driver.title)
+print(driver.current_url)
+print(driver.page_source)
+driver.quit()
